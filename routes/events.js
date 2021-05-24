8bb425ecdf42e7aae307641ec43ecd2d3273f77c
you know who <chorvickprocode@gmail.com>
2021-05-23 22:11:02 -0400

    mongodb schema built- server starts- organized client vs server files

---
+++ routes/events.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const db = require("../model/index");
+
+
+
+
+
+
+
+
+
+
+module.exports = router
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const db = require("../model");
+const passport = require("../passport");
+
+
+
+
+
+
+module.exports = router;
