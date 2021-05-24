a60df0a44e3bdc5b65314f071b1b10d6418118ea
Brian Flieck <brianflieck@gmail.com>
2021-05-24 19:12:12 -0400

    updated homepage sign in, to react 'link'

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 function HomePage() {
@@ -9,7 +10,7 @@ function HomePage() {
         <div style={{ margintop: "200px" }} className="jumbotron">
           <h2 className="display-4">Flock</h2>
           <h5 className="mt-4">a place for friends</h5>
-          <a className="btn btn-primary btn-lg mt-4" href="login.html" role="button">Log In</a>
+          <Link className="btn btn-primary btn-lg mt-4" to="/Login">Sign in</Link>
         </div>
       </div>
     </main>
