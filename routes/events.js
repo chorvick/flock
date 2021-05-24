e1d53779a2ee9c00ab5b72625097a6289ab1244b
you know who <chorvickprocode@gmail.com>
2021-05-23 23:52:33 -0400

    added server side route to pull all events and users

--- routes/events.js
+++ routes/events.js
@@ -2,12 +2,19 @@ const router = require("express").Router();
 const db = require("../model/index");
 
 
+///pull all events 
+
+
+router.get("/event", function (req, res) {
+    db.Events.find({}).then((response) => {
+        console.log(response);
+        console.log("event listing");
+
+    });
+
+});
 
 
 
 
-
-
-
-
-module.exports = router
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,11 @@ const db = require("../model");
 const passport = require("../passport");
 
 
+// pull all users 
+router.get("/user", function (req, res) {
+    db.Users.find({})
+        .then((response) => res.json(response))
+});
 
 
 
