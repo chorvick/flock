9b7b2edbb481135547727bf83f0b7c2e7bde47d0
you know who <chorvickprocode@gmail.com>
2021-05-27 18:29:10 -0400

    api routes tested fully work for post and get for user

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,11 @@
 const path = require("path");
-
-
 const router = require("express").Router();
 const db = require("../model/index");
-
-
-
-
 const userRoutes = require("./user");
-// const eventRoutes = require("./event")
-//// warning -- do not change this line of code or everything will break -- it's magic
 
-
-
-
-
-router.use(userRoutes);
-
-// router.use(eventRoutes);
+router.use("/users", userRoutes);
 
 // If no API routes return to react
-
 router.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../../client/public/index.html"));
 });
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {Users} = require("../model");
+const { Users } = require("../model");
 const passport = require("../passport");
 const mongojs = require("mongojs");
 
@@ -13,15 +13,20 @@ const mongojs = require("mongojs");
 //     console.log("Database Error:", error);
 //   });
 
-  router.get("/", (req, res) => {
-    res.send("Hello world");
-  });
+// router.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
 
+router.post("/new", (req, res) => {
+  Users.create(req.body).then(response => res.json({ data: response }))
+    .catch(err => res.json(err))
 
-  router.get("/user", function (req, res) {
-    Users.find({})
-    .then((response) => console.log(response))
-        // .then((response) => res.json(response))
+})
+router.get("/all", (req, res) => {
+  Users.find({})
+    .then((response) => res.json(response))
+    .catch((err) => res.setStatus(500).json(err))
+  // .then((response) => res.json(response))
 });
 
 
