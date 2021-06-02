a399003a0acf22d17c6dd384f6ebd3bbd4f4af8a
Mohammad Rouf <mrouf82@gmail.com>
2021-06-02 16:44:09 -0400

    added about page

--- client/app/index.js
+++ client/app/index.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import App from './components/App/App';
-import NotFound from './components/App/NotFound';
-import Home from './components/Home/Home';
-import Signin from './components/Home/Signin';
-import Signup from './components/Home/Signup';
-import Event from './components/Home/Event';
-import Logout from './components/Home/Logout';
+import App from "./components/App/App";
+import NotFound from "./components/App/NotFound";
+import Home from "./components/Home/Home";
+import Signin from "./components/Home/Signin";
+import Signup from "./components/Home/Signup";
+import Event from "./components/Home/Event";
+import Logout from "./components/Home/Logout";
+import AboutPage from "./components/AboutPage/AboutPage";
+//import "./components/AboutPage/about.css";
+import HelloWorld from "./components/HelloWorld/HelloWorld";
 
-import HelloWorld from './components/HelloWorld/HelloWorld';
-
-import './styles/styles.scss';
+import "./styles/styles.scss";
 import "materialize-css";
 
-render((
+render(
   <Router>
     <App>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/signin" component={Signin}/>
-        <Route path="/signup" component={Signup}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
         {/* <Route path="/helloworld" component={HelloWorld}/> */}
-        <Route path="/event" component={Event}/>
-        <Route path="/logout" component={Logout}/>
-        <Route component={NotFound}/>
+        <Route path="/event" component={Event} />
+        <Route path="/AboutPage" component={AboutPage} />
+        <Route path="/logout" component={Logout} />
+
+        <Route component={NotFound} />
       </Switch>
     </App>
-  </Router>
-), document.getElementById('app'));
+  </Router>,
+  document.getElementById("app")
+);
